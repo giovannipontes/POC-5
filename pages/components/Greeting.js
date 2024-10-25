a4378ce32a1ce76af3d1e390730cb4a9f6d74ac1
giovannipontes <118862566+giovannipontes@users.noreply.github.com>
2024-10-25 20:07:59 -0300

    Create Greeting.js

---
+++ pages/components/Greeting.js
@@ -0,0 +1,7 @@
+import styles from '../styles/Greeting.module.css';
+
+const Greeting = () => {
+  return <h1 className={styles.greeting}>Olá, bem-vindo à minha POC com Next.js e React!</h1>;
+};
+
+export default Greeting;
